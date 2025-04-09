0db54ed9f2bd47f44a83038d7e0f5c6be3bd25c3
agrshch <agrshch@gmail.com>
2025-04-09 17:29:27 +0200

    initial commit

--- code.ts
+++ code.ts
@@ -1,37 +1,147 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+const { PI, pow,sin, cos, abs, random, floor } = Math;
+const TAU = PI*2;
+let styles: TextStyle[] = [];
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    // This plugin creates rectangles on the screen.
-    const numberOfRectangles = msg.count;
-
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfRectangles; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+const UI_SETTINGS = {
+  width: 420, 
+  height: 600,
+  visible: true
 };
+
+figma.showUI(__html__, UI_SETTINGS);
+
+
+
+async function initialize() {
+  styles = await getStyles();
+}
+initialize();
+
+
+
+
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === 'apply') {
+    let waveFunction: Function = cylinder;
+    if(msg.style === 'cylinder') waveFunction = cylinder;
+    if(msg.style === 'wave') waveFunction = sineWave;
+    if(msg.style === 'random') waveFunction = () => Math.random();
+
+    await applyStylesToText(waveFunction, msg.applyTo);
+  }
+};
+
+
+
+
+
+async function applyStylesToText(waveFunction: Function, applyTo: String) {
+  const selection = figma.currentPage.selection;
+  if (selection.length === 0) {
+    figma.notify('Please, select a text object');
+    return;
+  }
+  
+  try {
+    // Обрабатываем каждый выделенный объект
+    for (const node of selection) {
+      if (node.type !== 'TEXT') {
+        figma.notify('Please, select a text object');
+        continue;
+      }
+
+      const text = node as TextNode;
+      await figma.loadFontAsync(text.fontName as FontName);
+      const lines = text.characters.split('\n').map(line => line.trim());
+      text.characters = lines.join('\n');
+
+      if(applyTo === 'letters'){
+        let charNum = 0;
+        for(let line of lines){
+          const phase = random()*TAU;
+          // Применяем стили к каждому символу
+          for (let i = 0; i < line.length; i++) {
+            const wave = waveFunction(line.length, i, phase)
+            const styleIndex = floor(wave * (styles.length-1));
+            const style = styles[styleIndex];
+            await text.setRangeTextStyleIdAsync(charNum, charNum + 1, style.id);
+            charNum ++;
+          }
+          charNum ++;
+        }
+      }
+      
+      if(applyTo === 'words'){
+        let charNum = 0;
+        for(let line of lines){
+          const words = line.split(' ');
+          const phase = random()*TAU;
+          // Применяем стили к каждому слову
+          for(let i in words){
+            const word = words[i];
+            const wave = waveFunction(words.length, i, phase);
+            const styleIndex = floor(wave * (styles.length-1));
+            const style = styles[styleIndex];
+            await text.setRangeTextStyleIdAsync(charNum, charNum + word.length, style.id);
+            charNum += word.length;
+            charNum ++; // white spaces and line breaks
+          }
+          // charNum ++;
+        }
+      }
+      
+
+      // После применения всех стилей, отвязываем их, сохраняя форматирование
+      for (let i = 0; i < text.characters.length; i++) {
+        await text.setRangeTextStyleIdAsync(i, i + 1, '');
+      }
+    }
+
+  } catch (error) {
+    console.error('Error:', error);
+    figma.notify('Произошла ошибка');
+  } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function cylinder(length: number, index: number): number {
+  const step = PI / (length - 1);
+  return 1-abs(cos(PI+step * index));
+}
+
+function sineWave(length: number, index: number, phase:number): number {
+  const step = PI / (length - 1);
+  const angle = phase + step*index;
+  return (1+cos(angle))/2;
+}
+
+
+
+
+
+async function getStyles(){
+  const styles = await figma.getLocalTextStylesAsync();
+  const wStyles = styles.filter(style => /^wdth(?:1[0-4][0-9]|150|[5-9][0-9])$/.test(style.name));
+
+  if (wStyles.length < 101) {
+    figma.notify('Не найдены стили');
+    return [];
+  }
+  return wStyles;
+}
+
+
+
+
+
